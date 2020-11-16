e8c790d9473134ab964ad3f5fb6074967875f9af
Tumicz <tumiczka@gmail.com>
2020-11-16 20:19:38 +0100

    Arithmetic value-giving operations project was added.

---
+++ week-4/day-1/Arithmetic value-giving operations/src/com/greenfoxaxademy/Main.java
@@ -0,0 +1,17 @@
+package com.greenfoxaxademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Swap
+        // Cseréld meg a 2 változó értékét programozás útján.
+        // Az a értéke legyen az, ami a b változóban van és fordítva.
+        int a = 123;
+
+        int b = 526;
+
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
