276fad82ed63a0629d57dbd7b563172689f8b154
Tumicz <tumiczka@gmail.com>
2020-11-16 21:40:00 +0100

    VariableMutation task was started.

--- week-4/day-1/Arithmetic value-giving operations/src/com/greenfoxaxademy/Main.java
+++ week-4/day-1/Arithmetic value-giving operations/src/com/greenfoxaxademy/Main.java
@@ -6,15 +6,13 @@ public class Main {
         //Swap
         // Cseréld meg a 2 változó értékét programozás útján.
         // Az a értéke legyen az, ami a b változóban van és fordítva.
-        int a = 123;
+        /*int a = 123;
         int b = 526;
         int c = a;
         a = b;
         b = c;
-
-        //BMI
         System.out.println(a);
-        System.out.println(b);
+        System.out.println(b);*/
         double massInKg = 91.2;
         double heightInM = 1.67;
         // Számíttasd és írasd ki a testtömeg-indexet (BMI)
@@ -40,7 +38,60 @@ public class Main {
         double heigth = 1.67;
         System.out.println("Magasságom: " + heigth + " m");
         boolean maritalStatus = true;
-        System.out.println("Házas vagyok-e: "+maritalStatus);
+        System.out.println("Házas vagyok-e: " + maritalStatus);
+
+        //VariableMutation
+        int a = 3;
+        // Növeld meg az "a" változó értékét 10-zel
+        a += 10;
+        System.out.println(a);
+        int b = 100;
+        // Csökkentsd a "b" változó értékeét 7-tel
+        b -= 7;
+        System.out.println(b);
+
+        int c = 44;
+        // A c értéke legyen dupla akkora
+        c *=2;
+        System.out.println(c);
+
+        int d = 125;
+        // Oszd el a d-t 5-tel
+        d /=5;
+        System.out.println(d);
+
+        int e = 8;
+        // Emeld köbre az e változó értékét
+        e *= e*e;
+        System.out.println(e);
+
+        int f1 = 123;
+        int f2 = 345;
+        // Logikai (boolean) értékként írasd ki hogy f1 nagyobb-e f2-nél
+        if (f1>f2){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
+        int g1 = 350;
+        int g2 = 200;
+        // Mondja meg a program, hogy g2 duplája nagyobb-e g1-nél (boolean)
+        if(g2*2>g1){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+        int h = 135798745;
+        // Mondja meg a program, hogy osztható-e 11-el maradék nélkül?
+        // Írasd is ki logikai (boolean) értékként
+        System.out.println("A h változó osztható-e maradék nélkül 11-el?: "+(h%11==0 ? "true":"false"));
+
+        int i1 = 10;
+        int i2 = 3;
+        // Mondja meg a program hogy i1 nagyobb-e i2 négyzeténél ÉS ugyanakkor kisebb-e i2 köbénél (boolean)
+
+        int j = 1521;
+        // Írasd ki, hogy j osztható-e 3-mal vagy 5-tel (boolean)
 
     }
 }
