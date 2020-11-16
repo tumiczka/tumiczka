9d936d944e1d41f9f7521be732141f319b3e1df7
Tumicz <tumiczka@gmail.com>
2020-11-16 20:24:07 +0100

    Swap was implemented.

--- week-4/day-1/Arithmetic value-giving operations/src/com/greenfoxaxademy/Main.java
+++ week-4/day-1/Arithmetic value-giving operations/src/com/greenfoxaxademy/Main.java
@@ -7,9 +7,10 @@ public class Main {
         // Cseréld meg a 2 változó értékét programozás útján.
         // Az a értéke legyen az, ami a b változóban van és fordítva.
         int a = 123;
-
         int b = 526;
-
+        int c=a;
+        a=b;
+        b=c;
 
         System.out.println(a);
         System.out.println(b);
