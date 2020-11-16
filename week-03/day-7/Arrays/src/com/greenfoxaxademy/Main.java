88e75f0bc1541aed439cfdb7b71107dc230af1f0
Tumicz <tumiczka@gmail.com>
2020-11-16 19:13:13 +0100

    Arrays and HelloMe was added.

--- week-03/day-7/Arrays/src/com/greenfoxaxademy/Main.java
+++ week-03/day-7/Arrays/src/com/greenfoxaxademy/Main.java
@@ -1,8 +1,24 @@
 package com.greenfoxaxademy;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //System.out.println("Hello!");
+        /*String[] names = {"Janos", "Bela", "Éva"};
+        for (String name : names) {
+            System.out.println(name);*/
+        int[][] table = new int[4][4];
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(Arrays.toString(table[i]));
+        }
+        int[][] differentLengthArray = new int[4][];
+        for (int i = 0; i < differentLengthArray.length; i++) {
+            differentLengthArray[i] = new int[i + 1];
+        System.out.println(Arrays.toString(differentLengthArray[i]));
+        }
     }
+
+
 }
---
+++ week-4/day-1/HelloMe/src/com/greenfoxaxademy/Main.java
@@ -0,0 +1,9 @@
+package com.greenfoxaxademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //Üdvözlet
+    }
+}
