b8a362eca0e90b299d186ed83bce5ff8c4988111
Tumicz <tumiczka@gmail.com>
2020-11-16 20:02:47 +0100

    CodingHours task was sloved.

--- week-4/day-1/HelloMe/src/com/greenfoxaxademy/Main.java
+++ week-4/day-1/HelloMe/src/com/greenfoxaxademy/Main.java
@@ -30,16 +30,29 @@ public class Main {
         // Írd át a fenti számot a kedvenc számoddá
         // És utána írasd ki efféle képpen: "A kedvenc számom: 8"
         // Kiíratáskor használd fel a favoriteNumber változót
-        favoriteNumber=9;
-        System.out.println("A kedvenc számom: "+favoriteNumber);
+        favoriteNumber = 9;
+        System.out.println("A kedvenc számom: " + favoriteNumber);
         // Two Numbers
-        int firstNumber=13;
-        int secondNumber=22;
-        System.out.println(firstNumber+secondNumber);
-        System.out.println(firstNumber-secondNumber);
-        System.out.println(firstNumber*secondNumber);
-        System.out.println(firstNumber/secondNumber);
-        System.out.println(13./secondNumber);
-        System.out.println(firstNumber%secondNumber);
+        int firstNumber = 13;
+        int secondNumber = 22;
+        System.out.println(firstNumber + secondNumber);
+        System.out.println(firstNumber - secondNumber);
+        System.out.println(firstNumber * secondNumber);
+        System.out.println(firstNumber / secondNumber);
+        System.out.println(13. / secondNumber);
+        System.out.println(firstNumber % secondNumber);
+        //CodingHours
+        // Írd ki, hogy a félév hány százalékát tölti a hallgató kódolással
+        // ha az átlagos heti munkaidő 52 óra
+        int codingPerDay = 6;
+        int codingPerWeek = codingPerDay * 5; // 30 óra hetente
+        int semesterLength = 17;
+        int codingPerSemester = codingPerWeek * semesterLength;
+        System.out.println(codingPerSemester);
+        double averageWeeklyWorkingTime = 52;
+        double percentValue = 30 * 100 / averageWeeklyWorkingTime;
+        System.out.println("A félév során kódolással töltött idő: " +percentValue+ " %");
+
+
     }
 }
