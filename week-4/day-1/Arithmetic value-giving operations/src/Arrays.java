487ff19f6acd223bf01932a19f54ecfc8a9b5afd
Tumicz <tumiczka@gmail.com>
2020-11-17 18:55:51 +0100

    twoDimensionalArray was written out

--- week-4/day-1/Arithmetic value-giving operations/src/Arrays.java
+++ week-4/day-1/Arithmetic value-giving operations/src/Arrays.java
@@ -9,9 +9,24 @@ public class Arrays {
         }
         System.out.println();*/
 
-        String[] Felsorolas={"répa", "retek", "mogyoró"};
-        for (String s:Felsorolas){
+        String[] Felsorolas = {"répa", "retek", "mogyoró"};
+        for (String s : Felsorolas) {
             System.out.println(s);
         }
+        //0    1    2
+        //3    4    4
+        //0    0    0
+        int[][] twoDimensionalArray= new int[3][3];
+        int[][] array={
+                {0, 1, 2},
+                {3, 4, 4},
+                {0, 0, 0},
+        };
+        for(int i=0; i<=2; i++){
+            for(int j=0; j<=2; j++){
+                System.out.print((array[i][j]+" "));
+            }
+        }
+
     }
 }
