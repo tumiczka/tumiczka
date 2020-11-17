3b631a50a166012a55f38be6d9bf73354f9f757f
Tumicz <tumiczka@gmail.com>
2020-11-17 18:36:05 +0100

    Arrays.java was added.

---
+++ week-4/day-1/Arithmetic value-giving operations/src/Arrays.java
@@ -0,0 +1,17 @@
+public class Arrays {
+    public static void main(String[] args) {
+        /*int[] tomb = new int[10];
+        for (int i = 0; i < tomb.length; i++) {
+            tomb[i] = (int) (Math.random() * 100 - 1);
+        }
+        for (int i = 0; i < tomb.length; i++) {
+            System.out.println(tomb[i]+" ");
+        }
+        System.out.println();*/
+
+        String[] Felsorolas={"répa", "retek", "mogyoró"};
+        for (String s:Felsorolas){
+            System.out.println(s);
+        }
+    }
+}
